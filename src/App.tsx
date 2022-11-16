7a45fba31b43559ebe1907294d7e9f9b986e7b09
aledale <ale.salas271102@gmail.com>
2022-11-15 23:53:12 -0300

    routers, store view, currency utility

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Container } from 'react-bootstrap'
 import { Home } from './pages/Home'
 import { Store } from './pages/Store'
 import { About } from './pages/About'
+import { Navbar } from './components/Navbar'
 
 function App () {
-  return <Container>
+  return <>
+  <Navbar />
+  <Container>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/store' element={<Store/>}></Route>
       <Route path='/about' element={<About/>}></Route>
     </Routes>
   </Container>
+  </>
 }
 
 export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,48 @@
-import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-import { BsCart4 } from 'react-icons/bs';
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { BsCart4 } from "react-icons/bs";
 
-export function Navbar () {
-    return (
-    <NavbarBs className='bg-white shadow-sm sb-3'>
-        <Container>
-            <Nav className='me-auto'>
-                <Nav.Link to='/' as={NavLink}>Home</Nav.Link>
-                <Nav.Link to='/store' as={NavLink}>Store</Nav.Link>
-                <Nav.Link to='/about' as={NavLink}>About</Nav.Link>
-            </Nav>
-            <Button style={{width: "3rem", height:"3rem", position:"relative"}} variant="outline-primary" className='rounded-circle'>
-                <BsCart4 />
-                <div className='rounded-circle bg-danger d-flex justify-content-center aling-items-center'>3</div>
-            </Button>
-        </Container>
+export function Navbar() {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm sb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <BsCart4 className="fs-4" />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.3rem",
+              height: "1.3rem",
+              position: "absolute",
+              bottom: "0",
+              right: "0",
+              transform:"translate(25%,15%)"
+            }}
+          >
+            12
+          </div>
+        </Button>
+      </Container>
     </NavbarBs>
-    )
+  );
 }
+
+//react router links
+//bs components
+//inline styles
